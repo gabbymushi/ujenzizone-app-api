da33c38e3cd64cb6b528b67c5869b91828fa2a4b
Gabriel Patrick <gabrielpatrickmushi@gmail.com>
2020-01-25 21:11:13 +0300

    move middleware on top of routes

--- index.js
+++ index.js
@@ -1,9 +1,12 @@
 const express = require('express');
 const db = require('./config/database');
 const routes = require('./modules');
+const middleware = require('./config/middleware');
 const app = express();
 db();
+middleware(app);
 routes(app);
+
 const port = process.env.PORT || 4500
 app.listen(port, () => {    
     console.log(`Ujenzi zone app is running on port ${port}`)
