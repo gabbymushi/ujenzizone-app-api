61f0d80b94870aca54f1fb7b2f77295df2a0d7db
Gabriel Patrick <gabrielpatrickmushi@gmail.com>
2020-01-25 14:17:39 +0300

    start db connection

--- index.js
+++ index.js
@@ -1,6 +1,7 @@
 const express = require('express');
+const db = require('./config/database');
 const app = express();
-
+db();
 const port = process.env.PORT || 4500
 app.listen(port, () => {
     console.log(`Ujenzi zone app is running on port ${port}`)
