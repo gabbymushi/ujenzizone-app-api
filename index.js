946a81061480a5b2b9803ed8a6175434ef32b78a
Gabriel Patrick <gabrielpatrickmushi@gmail.com>
2020-01-25 18:28:10 +0300

    pass app instance to routes

--- index.js
+++ index.js
@@ -1,8 +1,10 @@
 const express = require('express');
 const db = require('./config/database');
+const routes = require('./modules');
 const app = express();
 db();
+routes(app);
 const port = process.env.PORT || 4500
-app.listen(port, () => {
+app.listen(port, () => {    
     console.log(`Ujenzi zone app is running on port ${port}`)
 })
