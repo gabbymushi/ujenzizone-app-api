46c4cb6e28bbb16014b70cfdb4c3934f720fa0c1
Gabriel Patrick <gabrielpatrickmushi@gmail.com>
2020-01-25 21:11:53 +0300

    create function to view mafundi

--- modules/mafundi/mafundi.controller.js
+++ modules/mafundi/mafundi.controller.js
@@ -1,10 +1,22 @@
 const Mafundi = require('./mafundi.model');
 
+exports.getMafundi = async (req,res) => {
+    try {
+        //console.log('Body',req.body);
+       const fundi= await Mafundi.find({});
+       res.status(200).json(fundi);
+    } catch (e) {
+        //console.log(e.message);
+        res.status(500).json(e.message);
+    }
+}
 exports.create = async (req,res) => {
     try {
+       // console.log('Body',req.body);
        const fundi= await Mafundi.create(req.body);
        res.status(200).json(fundi);
     } catch (e) {
-        console.log(e.message);
+        //console.log(e.message);
+        res.status(500).json(e.message);
     }
 }
