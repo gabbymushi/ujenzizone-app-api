933b26ea04f6480c05b6611ff50babba918021b7
Gabriel Patrick <gabrielpatrickmushi@gmail.com>
2020-02-09 19:56:38 +0300

    create rout to create categories

--- modules/category/category.routes.js
+++ modules/category/category.routes.js
@@ -3,5 +3,6 @@ const router = new Router();
 const CategoryController = require('./category.controller');
 
 router.get('/',CategoryController.geCategories);
+router.post('/', CategoryController.createCategory);
 
 module.exports = router;
