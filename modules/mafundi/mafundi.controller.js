70c2782150fa176e1b91ebdce250d880cbd7a6d8
Gabriel Patrick <gabrielpatrickmushi@gmail.com>
2020-02-09 13:42:18 +0300

    remove unused comments

--- modules/mafundi/mafundi.controller.js
+++ modules/mafundi/mafundi.controller.js
@@ -2,21 +2,17 @@ const Mafundi = require('./mafundi.model');
 
 exports.getMafundi = async (req,res) => {
     try {
-        //console.log('Body',req.body);
        const fundi= await Mafundi.find({});
        res.status(200).json(fundi);
     } catch (e) {
-        //console.log(e.message);
         res.status(500).json(e.message);
     }
 }
 exports.create = async (req,res) => {
     try {
-       // console.log('Body',req.body);
        const fundi= await Mafundi.create(req.body);
        res.status(200).json(fundi);
     } catch (e) {
-        //console.log(e.message);
         res.status(500).json(e.message);
     }
 }
