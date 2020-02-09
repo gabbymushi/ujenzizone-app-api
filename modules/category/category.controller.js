238b1e6577e398cca2bce18c32edb0eeceb14b63
Gabriel Patrick <gabrielpatrickmushi@gmail.com>
2020-02-09 19:51:29 +0300

    create a  function to create categories

--- modules/category/category.controller.js
+++ modules/category/category.controller.js
@@ -8,3 +8,11 @@ exports.geCategories = async (req,res) => {
         res.status(500).json(e.message);
     }
 }
+exports.createCategory = async (req,res) => {
+    try {
+       const category= await Category.create(req.body);
+       res.status(200).json(category);
+    } catch (e) {
+        res.status(500).json(e.message);
+    }
+}
