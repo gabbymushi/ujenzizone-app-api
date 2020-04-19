47e7695075187fc16068efc896b44bb359b3cd54
Gabriel Patrick <gabrielpatrickmushi@gmail.com>
2020-04-19 13:18:15 +0300

    store fundi-specific details

--- modules/mafundi/mafundi.service.js
+++ modules/mafundi/mafundi.service.js
@@ -1,17 +1,20 @@
 const mafundiRepository = require('./mafundi.repository');
+const userRepository = require('../users/user.repository');
 
 exports.getMafundi = async () => {
     try {
-       const fundi= await mafundiRepository.getMafundi();
-       return fundi;
+        const fundi = await mafundiRepository.getMafundi();
+        return fundi;
     } catch (e) {
         throw new Error(e.message);
     }
 }
 exports.create = async (body) => {
     try {
-       const fundi= await mafundiRepository.create(body);
-       return fundi;
+        const user = await userRepository.create(body);
+        const fundiDetails = { user: user._id, category };
+        const fundi = await mafundiRepository.create(fundiDetails);
+        return fundi;
     } catch (e) {
         throw new Error(e.message);
     }
