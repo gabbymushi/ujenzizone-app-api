403899bdec67839b70825d9f4123e256ec9bfe60
Gabriel Patrick <gabrielpatrickmushi@gmail.com>
2020-08-15 21:35:38 +0300

    add cors middleware

--- src/config/middleware.ts
+++ src/config/middleware.ts
@@ -2,8 +2,12 @@ import * as dotenv from 'dotenv';
 import morgan from 'morgan';
 import bodyParser from 'body-parser';
 import express from 'express';
+import cors from 'cors';
 dotenv.config();
+
 export default (app: express.Application) => {
+    app.use(cors());
     app.use(bodyParser.json());
     app.use(morgan('dev'));
+    
 }
